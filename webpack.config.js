cb504428f5527a5bfb318c77f673905895bc1528
stobita <tobita.pc@gmail.com>
2017-12-31 16:02:22 +0900

    sass導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,57 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: path.join(__dirname, 'src/app.tsx'),
-  output: {
-    path: path.join(__dirname, 'www'),
-    filename: 'bundle.js',
-  },
-  devServer: {
-    contentBase: 'www',
-    port: 4000,
-    inline: true,
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx$/,
-        use: 'awesome-typescript-loader',
-        exclude: /node_modules/,
-      },
-      {
-        enforce: 'pre',
-        test: /\.tsx$/,
-        exclude: /node_modules/,
-        loader: 'tslint-loader',
-      },
+const extractSass = new ExtractTextPlugin('bundle.css');
+
+module.exports = [
+  {
+    entry: path.join(__dirname, 'src/app.tsx'),
+    output: {
+      path: path.join(__dirname, 'www'),
+      filename: 'bundle.js',
+    },
+    devServer: {
+      contentBase: 'www',
+      port: 4000,
+      inline: true,
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx$/,
+          use: 'awesome-typescript-loader',
+          exclude: /node_modules/,
+        },
+        {
+          enforce: 'pre',
+          test: /\.tsx$/,
+          exclude: /node_modules/,
+          loader: 'tslint-loader',
+        },
+      ],
+    },
+  }, {
+    entry: path.join(__dirname, 'src/styles/app.sass'),
+    output: {
+      path: path.join(__dirname, 'www'),
+      filename: 'bundle.css',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.sass$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader'],
+          }),
+        },
+      ],
+    },
+    plugins: [
+      extractSass,
     ],
-  },
-};
+  }];
